025af4816dc216d4dd7fd2bcf0915730bc093916
KitchiDioless <dustirris@gmail.com>
2025-12-06 23:09:10 +0300

    fix: fix

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
-export const API_BASE = import.meta.env.VITE_API_BASE;
+export const API_BASE = import.meta.env.VITE_API_BASE || 'https://ceramic-api.onrender.com';
 export const BASE_URL = import.meta.env.BASE_URL || '/web-lab3/';
 
