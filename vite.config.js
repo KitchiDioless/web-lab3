2fff514c7ead37954568feecd7d4a94472a44052
KitchiDioless <dustirris@gmail.com>
2025-12-06 16:11:12 +0300

    fix: fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-	base: '/lab3/',
+	base: '/web-lab3/',
 	build: {
 		rollupOptions: {
 			input: {
